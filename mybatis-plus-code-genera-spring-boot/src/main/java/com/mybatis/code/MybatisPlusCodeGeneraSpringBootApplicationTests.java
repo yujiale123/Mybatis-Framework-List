6dabb3261bd2a92ea52e9163d893c727578ab818
yujiale <yu18371919563@163.com>
2021-12-07 15:38:56 +0800

    mybatisPlus基于SpringBoot的代码生成器

---
+++ mybatis-plus-code-genera-spring-boot/src/main/java/com/mybatis/code/MybatisPlusCodeGeneraSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mybatis.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yujiale
+ */
+@SpringBootApplication
+public class MybatisPlusCodeGeneraSpringBootApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusCodeGeneraSpringBootApplicationTests.class, args);
+    }
+
+}
